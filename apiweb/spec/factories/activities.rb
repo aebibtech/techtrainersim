df3d6875b8c7d46a457154c5c06bb526817909c4
aebibtech <paul.camano@gmail.com>
2023-04-20 16:22:31 +0800

    Added Activity model

--- apiweb/spec/factories/activities.rb
+++ apiweb/spec/factories/activities.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :activity do
-    category { nil }
-    activity_name { "MyString" }
-    description { "MyText" }
-    activity_image { "MyString" }
-    reward { 1 }
+    category { build(:category) }
+    activity_name { "Taking a picture" }
+    description { "This learning activity will teach you how to take pictures using your Android phone." }
+    activity_image { "taking-a-picture.png" }
+    reward { 10 }
   end
 end
